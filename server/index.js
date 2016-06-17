020a09ac7cc9a4d31f7dc0a6d53a94f2544d993c
patw <patw.hi@gmail.com>
2016-06-17 21:51:08 +0800

    Added router setting

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App setup
 app.use(morgan('combined'));  // HTTP request logger middleware
 app.use(bodyParser.json({ type: '*/*' }));  // parse incoming request middleware
+router(app);
 
 // Server setup
 const port = process.env.PORT || 8080;
