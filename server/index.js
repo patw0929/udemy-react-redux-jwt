94daa30b1ff8f6ebe35be59fb51208e55949bd54
patw <patw.hi@gmail.com>
2016-06-17 21:38:22 +0800

    Express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+
+
+// Server setup
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening:', port);
