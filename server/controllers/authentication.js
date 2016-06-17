326f6064c942749c57f5f693cb6474bb8ca4b647
patw <patw.hi@gmail.com>
2016-06-17 23:37:49 +0800

    Create JWT token

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,13 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  // sub - subject... -> who
+  // iat - issue at time
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
@@ -31,7 +40,7 @@ exports.signup = function (req, res, next) {
       }
 
       // response success msg
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 }
