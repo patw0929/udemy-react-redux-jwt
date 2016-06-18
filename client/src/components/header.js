ba698b846d0f7b9ae1b200221699f1934cb33cd4
patw <patw.hi@gmail.com>
2016-06-18 21:15:25 +0800

    Render different links depend on authenticated status

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-export default class Header extends Component {
+class Header extends Component {
   constructor(props) {
     super(props);
+  }
+
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign out</Link>
+        </li>
+      );
+    }
+
+    return [
+      <li key={'nav-signin'} className="nav-item">
+        <Link className="nav-link" to="/signin">Sign in</Link>
+      </li>,
+      <li key={'nav-signup'} className="nav-item">
+        <Link className="nav-link" to="/signup">Sign up</Link>
+      </li>
+    ];
   }
 
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">React Redux JWT Practice App</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            <Link to="/signin">Sign in</Link>
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated,
+  };
+}
+
+export default connect(mapStateToProps)(Header);
