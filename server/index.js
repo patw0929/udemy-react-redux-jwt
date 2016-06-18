3c18a22e75ac78a7dda5ea200a38ae1fda5af378
patw <patw.hi@gmail.com>
2016-06-18 19:46:52 +0800

    Setuping up the cors in server

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/udemy_react_redux_jwt');
 
 // App setup
 app.use(morgan('combined'));  // HTTP request logger middleware
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));  // parse incoming request middleware
 router(app);
 
